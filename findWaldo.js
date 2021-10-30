4edf3cbd4635aa802b2b98e6c4994f23b5117be1
valik94 <valentin.lyashenko@ryerson.ca>
2021-10-30 20:56:06 +0000

    committing findWaldo function that actually implements step 2 of using forEach callback function instead of the for loop used in step 1. forEAch callback function is a lot more useful because it breaks up the index and value inside the function and you can access them directly. Furthermore, you are passing behaviour into the next function.

--- findWaldo.js
+++ findWaldo.js
@@ -1,15 +1,28 @@
 // The second argument/parameter is expected to be a (callback) function
-const findWaldo = function(index,found) {
-  for (let i = 0; i < index.length; i++) {
-    let name = index[i];
-    if (name === "Waldo") {
-      found(i);   // execute callback
-    }
-  }
+const findWaldo = function(array,found) {
+  array.forEach(function(value,i) { //forEach is a callback function within itself
+    if (value === "Waldo"){
+      found(i); // execute callback
+       }   
+  });
 }
+
 
 const actionWhenFound = function(index) {
   console.log(`Found Waldo at index ${index}!`);
 }
 
 findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+
+
+// var myArray = [123, 15, 187, 32];
+
+// myArray.forEach(function (value, i) { //Callback function which has two arguments i - Index, Value -= Value in the Array
+//     console.log(i, value);
+// });
+
+// // Outputs:
+// // 0: 123
+// // 1: 15
+// // 2: 187
+// // 3: 32
